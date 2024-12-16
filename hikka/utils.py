97e03f0740bd1e89b8401a866eeadbdcd95e696f
Afteroid <131268022+Afteroid@users.noreply.github.com>
2024-12-16 19:24:35 +0500

    Update utils.py

--- hikka/utils.py
+++ hikka/utils.py
@@ -941,7 +941,7 @@ def get_named_platform() -> str:
     if main.IS_CODESPACES:
         return "🐈‍⬛ Codespaces"
 
-    return f"✌️ lavHost {os.environ['LAVHOST']}" if main.IS_LAVHOST else "💎 VDS"
+    return f"✌️ lavHost {os.environ['LAVHOST']}" if main.IS_LAVHOST else "💎 S | host"
 
 
 def get_platform_emoji() -> str:
